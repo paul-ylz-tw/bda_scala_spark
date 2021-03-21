0b8f7e33cdd1868bd188de084e1b7c5d72209479
Paul Yeoh <paulyeoh@thoughtworks.com>
2021-03-21 15:56:57 +0800

    add unit test for groupedPostings

--- week2_stackoverflow/src/test/scala/stackoverflow/StackOverflowSuite.scala
+++ week2_stackoverflow/src/test/scala/stackoverflow/StackOverflowSuite.scala
@@ -38,6 +38,27 @@ class StackOverflowSuite {
     assert(instantiatable, "Can't instantiate a StackOverflow object")
   }
 
+  //  case class Posting(postingType: Int, id: Int, acceptedAnswer: Option[Int], parentId: Option[QID], score: Int, tags: Option[String]) extends Serializable
+
+  @Test def `groupedPostings groups questions and answers by question ID`: Unit = {
+    val data = Seq(
+      Posting(1, 123, Some(456), None, 0, Some("C#")),
+      Posting(2, 234, None, Some(123), 5, Some("C#")),
+      Posting(2, 456, None, Some(123), 11, Some("C#")),
+      Posting(1, 1000, None, None, 0, Some("Java")),
+      Posting(2, 1001, None, Some(1000), 5, Some("Java")),
+      Posting(2, 1002, None, Some(1000), 11, Some("Java")),
+      Posting(2, 1003, None, Some(1000), 12, Some("Java")),
+    )
+    val postingsRdd = sc.parallelize(data)
+    val so = new StackOverflow
+    val grouped = so.groupedPostings(postingsRdd).collect()
+    assertEquals(2, grouped.length)
+    assertEquals(123, grouped(0)._1)
+    assertEquals(1000, grouped(1)._1)
+    assertEquals(2, grouped(0)._2.toList.size)
+    assertEquals(3, grouped(1)._2.toList.size)
+  }
 
 
   @Rule def individualTestTimeout = new org.junit.rules.Timeout(100 * 1000)
